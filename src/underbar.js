8e28b6b6c58b9abeb4a2b06fc3b94a0aee60de16
Andrew Kishino <AndrewKishino@gmail.com>
2015-05-18 18:00:30 -0700

    Commit _.some function after passing tests

--- src/underbar.js
+++ src/underbar.js
@@ -202,9 +202,15 @@
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
-    // TIP: There's a very clever way to re-use every() here.
+    function isntSome(value) {
+      if (iterator === undefined) {
+        return !(value);
+      } else {
+        return !(iterator(value));
+      }
+    }
+    return !_.every(collection, isntSome);
   };
-
 
   /**
    * OBJECTS
