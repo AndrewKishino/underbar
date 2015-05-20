a82febabb35edee5f12c26b16c565fae12e2d743
Andrew Kishino <AndrewKishino@gmail.com>
2015-05-19 19:56:14 -0700

    Commit _.extend after passing tests

--- src/underbar.js
+++ src/underbar.js
@@ -231,6 +231,12 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    _.each(arguments, function(source) {
+      for (var prop in source) {
+        obj[prop] = source[prop];
+      }
+    });
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
